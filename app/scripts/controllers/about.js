25c3c0c3ee113b689f0061863a790aaccdaa8eb8
randreucetti <ross.andreucetti@gmail.com>
2015-12-04 21:40:17 +0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ranelaghfcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ranelaghfcApp
+ */
+angular.module('ranelaghfcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ranelaghfcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ranelaghfcApp
+ */
+angular.module('ranelaghfcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
